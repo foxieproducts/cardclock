ae9eba69707199cc09ca5cf1526a550e382df6ac
Will Fox <alex@neg.plus>
2021-09-08 13:14:18 -0500

    Improved light sensor class further, still working on it

--- firmware/src/light_sensor.hpp
+++ firmware/src/light_sensor.hpp
@@ -2,26 +2,33 @@
 #include <Arduino.h>         // for map()
 #include <stdint.h>          // for uint16_t and others
 #include <user_interface.h>  // for ESP-specific API calls
+#include "elapsed_time.hpp"
 
 /**
- * This class uses the ESP API to read multiple samples from the ADC.
- * The main reason this is used instead of analogRead() is because
- * analogRead() is unstable on the ESP8266 and crashes often (???)
+ * This class solves two problems:
+ * 1. It uses the ESP API to read multiple samples from the ADC.
+ *    The main reason this is used instead of analogRead() is because
+ *    analogRead() is unstable on the ESP8266 and crashes often (???)
+ * 2. It provides averaging so that light changes happen gradually,
+ *    which is also useful to eliminate LED-lamp-PWM-flicker
  * */
 class LightSensor {
   public:
     enum {
-        ADC_SAMPLES = 8,
+        ADC_SAMPLES = 4,
         ADC_CLOCK_DIVIDER = 8,
 
-        MIN_BRIGHTNESS = 10,
+        MIN_BRIGHTNESS = 4,
         MAX_BRIGHTNESS = 150,
-        MAX_JITTER = 3,
+
+        HISTORY_SIZE = 40,
+        MAX_JITTER = 4,
     };
 
   private:
     uint16_t m_samples[ADC_SAMPLES] = {0};
-    uint16_t m_cur = 0;
+    uint16_t m_history[HISTORY_SIZE] = {0};
+    int m_pos{0};
 
   public:
     LightSensor() { Get(); }
@@ -33,39 +40,37 @@ class LightSensor {
         for (int i = 0; i < ADC_SAMPLES; i++) {
             average += m_samples[i];
         }
+        average /= ADC_SAMPLES;
+        average = map(average, 10, 600, MIN_BRIGHTNESS, MAX_BRIGHTNESS);
+        average = constrain(average, MIN_BRIGHTNESS, MAX_BRIGHTNESS);
 
-        return Constrain(average / ADC_SAMPLES);
+        if ((uint16_t)average > m_history[m_pos] + MAX_JITTER ||
+            (uint16_t)average < m_history[m_pos] - MAX_JITTER ||
+            average == MIN_BRIGHTNESS || average == MAX_BRIGHTNESS) {
+            m_history[m_pos] = average;
+        }
+        if (m_pos++ == HISTORY_SIZE) {
+            m_pos = 0;
+        }
+
+        average = 0;
+        for (int i = 0; i < HISTORY_SIZE; i++) {
+            average += m_history[i];
+        }
+        average /= HISTORY_SIZE;
+
+        return average;
     }
 
   private:
-    uint16_t Constrain(uint16_t value) {
-        value = map(value, 10, 600, MIN_BRIGHTNESS, MAX_BRIGHTNESS);
-        if (value < m_cur - MAX_JITTER || value > m_cur + MAX_JITTER) {
-            m_cur = constrain(value, MIN_BRIGHTNESS, MAX_BRIGHTNESS);
-        }
-
-        if (m_cur <= MIN_BRIGHTNESS + MAX_JITTER) {
-            m_cur = MIN_BRIGHTNESS;
-        } else if (m_cur >= MAX_BRIGHTNESS - MAX_JITTER) {
-            m_cur = MAX_BRIGHTNESS;
-        }
-
-        return m_cur;
-    }
-
     void ReadADC() {
-        for (int i = 0; i < ADC_SAMPLES; i++) {
-            system_adc_read_fast(m_samples, ADC_SAMPLES, ADC_CLOCK_DIVIDER);
-        }
-    }
-
-    // This doesn't seem to be necessary
-    void ReadADCWithoutInterrupts() {
         system_soft_wdt_stop();
         ets_intr_lock();
         noInterrupts();
 
-        ReadADC();
+        for (int i = 0; i < ADC_SAMPLES; i++) {
+            system_adc_read_fast(m_samples, ADC_SAMPLES, ADC_CLOCK_DIVIDER);
+        }
 
         interrupts();
         ets_intr_unlock();
