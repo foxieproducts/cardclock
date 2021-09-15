d807307b447bf6b5475abc35d1eb59ac9bbe4479
Will Fox <alex@neg.plus>
2021-09-15 18:10:05 -0500

    Add ability to show IP address from ConfigMenu

--- firmware/src/main.cpp
+++ firmware/src/main.cpp
@@ -19,6 +19,19 @@ void setup() {
     Display disp(settings);
     FoxieWiFi foxieWiFi(disp, settings);
 
+    pinMode(PIN_BTN_LEFT, INPUT_PULLUP);
+    if (digitalRead(PIN_BTN_LEFT) == LOW) {
+        // safe mode OTA on boot. Hopefully WiFi is configured!
+        disp.DrawText(1, "SAFE", ORANGE);
+        while (true) {
+            rtc.Update();
+            foxieWiFi.Update();
+            disp.Update();
+
+            delay(5);
+        }
+    }
+
     MenuManager menuMgr(disp, settings);
     menuMgr.Add(std::make_shared<TimeMenu>(disp, rtc, settings));   // menu 0
     menuMgr.Add(std::make_shared<ClockMenu>(disp, rtc, settings));  // menu 1
@@ -26,6 +39,12 @@ void setup() {
     auto configMenu = std::make_shared<ConfigMenu>(disp, settings);
     configMenu->Add({disp, settings, "HOUR_FMT", {"12", "24"}});
     configMenu->Add({disp, settings, "WIFI", {"OFF", "ON", "CFG"}});
+    configMenu->Add({disp, settings, "ADDR", [&]() {
+                         String ip = foxieWiFi.IsConnected()
+                                         ? WiFi.localIP().toString()
+                                         : "NOT CONNECTED";
+                         disp.DrawTextScrolling(ip, GREEN);
+                     }});
     menuMgr.Add(configMenu);  // menu 2
 
     menuMgr.ActivateMenu(1);  // clock menu
