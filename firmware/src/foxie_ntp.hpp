8db9a8975e649853220d51ebefe95cb10559191a
Will Fox <alex@neg.plus>
2021-09-16 16:00:26 -0500

    Cleanup

--- firmware/src/foxie_ntp.hpp
+++ firmware/src/foxie_ntp.hpp
@@ -23,32 +23,32 @@ class FoxieNTP {
 
     void Update() {
         if (WiFi.isConnected()) {
-            if (m_isInitialized && WiFi.isConnected()) {
-                m_ntpClient.update();
-                if (m_sinceLastUpdate.Ms() >= ONE_MINUTE) {
-                    UpdateRTCTime();
-                }
-            } else if (!m_isInitialized && WiFi.isConnected()) {
+            if (!m_isInitialized) {
                 m_ntpClient.begin();
                 m_ntpClient.setTimeOffset(m_settings["UTC"].as<int>() * 60 *
                                           60);
                 m_isInitialized = true;
+                m_ntpClient.update();
                 UpdateRTCTime();
+            } else {
+                m_ntpClient.update();
+                if (m_sinceLastUpdate.Ms() >= ONE_MINUTE) {
+                    UpdateRTCTime();
+                }
             }
         }
     }
 
     void UpdateRTCTime() {
-        const int offset = m_settings["UTC"] ? m_settings["UTC"].as<int>() : 0;
+        const int offset =
+            m_settings.containsKey("UTC") ? m_settings["UTC"].as<int>() : 0;
         m_ntpClient.setTimeOffset(offset * 60 * 60);
-        m_ntpClient.update();
         if (m_ntpClient.getHours() != m_rtc.Hour() ||
             m_ntpClient.getMinutes() != m_rtc.Minute() ||
             m_ntpClient.getSeconds() != m_rtc.Second()) {
             m_rtc.SetTime(m_ntpClient.getHours(), m_ntpClient.getMinutes(),
                           m_ntpClient.getSeconds());
             m_sinceLastUpdate.Reset();
-            // m_ntpClient.
         }
     }
 };
