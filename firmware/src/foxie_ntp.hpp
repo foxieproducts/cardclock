f70ce9880c11330384b21c2c752b1d73263a1632
Will Fox <alex@neg.plus>
2021-09-16 13:23:30 -0500

    Add UTC offset capability to FoxieNTP

--- firmware/src/foxie_ntp.hpp
+++ firmware/src/foxie_ntp.hpp
@@ -9,38 +9,38 @@
 class FoxieNTP {
   private:
     enum {
-        ONE_MINUTE_IN_MS = 60000,
-        FIVE_MINUTES_IN_MS = 300000,
+        ONE_MINUTE = 60000,
     };
     Settings& m_settings;
     Rtc& m_rtc;
     WiFiUDP m_udp;
-    NTPClient m_ntpClient{m_udp, "time.nist.gov", 0,  //-18000,
-                          int(ONE_MINUTE_IN_MS)};
+    NTPClient m_ntpClient{m_udp, "time.nist.gov", 0, int(ONE_MINUTE)};
     ElapsedTime m_sinceLastUpdate;
     bool m_isInitialized{false};
 
   public:
-    FoxieNTP(Settings& settings, Rtc& rtc) : m_settings(settings), m_rtc(rtc) {
-        m_ntpClient.begin();
-    }
+    FoxieNTP(Settings& settings, Rtc& rtc) : m_settings(settings), m_rtc(rtc) {}
 
     void Update() {
         if (WiFi.isConnected()) {
             if (m_isInitialized && WiFi.isConnected()) {
                 m_ntpClient.update();
-                if (m_sinceLastUpdate.Ms() >= ONE_MINUTE_IN_MS) {
-                    SetTimeFromNTP();
+                if (m_sinceLastUpdate.Ms() >= ONE_MINUTE) {
+                    UpdateRTCTime();
                 }
             } else if (!m_isInitialized && WiFi.isConnected()) {
                 m_ntpClient.begin();
+                m_ntpClient.setTimeOffset(m_settings["UTC"].as<int>() * 60 *
+                                          60);
                 m_isInitialized = true;
-                SetTimeFromNTP();
+                UpdateRTCTime();
             }
         }
     }
 
-    void SetTimeFromNTP() {
+    void UpdateRTCTime() {
+        const int offset = m_settings["UTC"] ? m_settings["UTC"].as<int>() : 0;
+        m_ntpClient.setTimeOffset(offset * 60 * 60);
         m_ntpClient.update();
         if (m_ntpClient.getHours() != m_rtc.Hour() ||
             m_ntpClient.getMinutes() != m_rtc.Minute() ||
